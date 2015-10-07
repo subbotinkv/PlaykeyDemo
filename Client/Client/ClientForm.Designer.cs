fa2a41bf9b0e284c006de1d6e6c61bf17a88f05c
Koc <K.Subbotin@ics.perm.ru>
2015-10-07 16:54:25 +0500

    Заготовка формы клиента.

---
+++ Client/Client/ClientForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Client
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(523, 329);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 50);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSendClick);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMessage.Location = new System.Drawing.Point(12, 329);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(505, 50);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // tbLog
+            // 
+            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLog.Location = new System.Drawing.Point(12, 12);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.Size = new System.Drawing.Size(586, 311);
+            this.tbLog.TabIndex = 2;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 391);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.btnSend);
+            this.Name = "ClientForm";
+            this.Text = "Клиент";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.TextBox tbLog;
+    }
+}
+
---
+++ Client/Client/ClientForm.cs
@@ -0,0 +1,17 @@
+﻿namespace Client
+{
+    using System.Windows.Forms;
+
+    public partial class ClientForm : Form
+    {
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSendClick(object sender, System.EventArgs e)
+        {
+            
+        }
+    }
+}
