0990c8712a9380dae6901c8d8eff37c8a3e6b05d
Koc <K.Subbotin@ics.perm.ru>
2015-10-09 11:50:54 +0500

    Revert "Синхронный прием сообщений на сервере."
    This reverts commit ea46d3c78f743701db96000334eeecc05936e8b7.

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,41 +1,10 @@
 ﻿namespace Server
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Net.Sockets;
-    using System.Text;
-
     class Program
     {
-        private const int Size = 1024;
-
-        private static void Main()
+        static void Main(string[] args)
         {
-            TcpListener listener = TcpListener.Create(11000);
-            listener.Start();
 
-            var clients = new List<TcpClient>();
-
-            while (true)
-            {
-                TcpClient client = listener.AcceptTcpClient();
-                if (!clients.Contains(client))
-                {
-                    clients.Add(client);
-                }
-
-                NetworkStream stream = client.GetStream();
-                var buffer = new byte[Size];
-                int offset = 0;
-                var builder = new StringBuilder();
-                while (stream.Read(buffer, offset, Size) != 0)
-                {
-                    builder.Append(Encoding.UTF8.GetString(buffer));
-                    offset += Size;
-                }
-
-                Console.WriteLine(builder.ToString());
-            }
         }
     }
 }
